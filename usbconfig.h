ed13695159f3506343b7373ab2a1e734f5c088c4
Christian Hirsch <christian.hirsch@tuwien.ac.at>
2018-11-19 10:42:56 +0100

    update usb device name

--- usbconfig.h
+++ usbconfig.h
@@ -248,8 +248,8 @@ section at the end of this file).
  * obdev's free shared VID/PID pair. See the file USB-IDs-for-free.txt for
  * details.
  */
-#define USB_CFG_DEVICE_NAME     'F','l','i','g','h','t','-','C','t','l'
-#define USB_CFG_DEVICE_NAME_LEN 10
+#define USB_CFG_DEVICE_NAME     'f','l','i','g','h','t','s','i','m','-','c','o','c','k','p','i','t'
+#define USB_CFG_DEVICE_NAME_LEN 17
 /* Same as above for the device name. If you don't want a device name, undefine
  * the macros. See the file USB-IDs-for-free.txt before you assign a name if
  * you use a shared VID/PID.
